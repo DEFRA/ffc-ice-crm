5e56c93a7519f3a5d767213e9c45890371e5f584
Jordan Vidal <jordan.vidal@cognizant.com>
2024-03-06 14:53:18 +0000

    feat: remove redundant files

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+require('dotenv').config()
 const server = require('./server')
 const ServiceBusQueue = require('./services/service-bus-queue')
 
--- app/server.js
+++ app/server.js
@@ -3,8 +3,7 @@ const Hapi = require('@hapi/hapi')
 const routes = require('./routes')
 
 const server = Hapi.server({
-  port: process.env.PORT,
-  host: process.env.NODE_ENV === 'development' && 'localhost'
+  port: process.env.PORT
 })
 
 const serverRoutes = [].concat(
