44f5fdb0e60db25ca092d7ff3366457ab39edfeb
Naveen Tamma <ntamma@hsdyn.com>
2025-06-20 15:11:48 +0100

    Suppress console output in production

--- app/index.js
+++ app/index.js
@@ -9,6 +9,16 @@ const init = async () => {
   MessageProcessorService.getInstance()
 }
 
+// Disable all console logging in non-development environments to prevent
+// sensitive logs or excessive output in production and pre environments.
+// This includes console.log, console.debug, console.info, and console.warn.
+if (process.env.NODE_ENV !== 'development') {
+  console.log = () => {}
+  console.debug = () => {}
+  console.info = () => {}
+  console.warn = () => {}
+}
+
 process.on('unhandledRejection', (err) => {
   console.log(err)
   process.exit(1)
